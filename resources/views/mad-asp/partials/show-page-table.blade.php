562e1914daa66adcd2e76fe0edde9f21fdb5b49a
MrBobPRO <bobnuridinov@gmal.com>
2025-02-20 11:39:46 +0500

    MAD ASP restyled

--- resources/views/mad-asp/partials/show-page-table.blade.php
+++ resources/views/mad-asp/partials/show-page-table.blade.php
@@ -28,11 +28,11 @@
             <th class="mad-asp-table__th--mah-name"></th>
 
             {{-- Plan for the year --}}
-            <th style="text-align: center">Кк {{ __('plan') }}</th>
-            <th style="text-align: center">Кк {{ __('fact') }}</th>
-            <th style="text-align: center">Кк %</th>
-            <th style="text-align: center">НПР {{ __('fact') }}</th>
-            <th style="text-align: center">НПР %</th>
+            <th>Кк {{ __('plan') }}</th>
+            <th>Кк {{ __('fact') }}</th>
+            <th>Кк %</th>
+            <th>НПР {{ __('fact') }}</th>
+            <th>НПР %</th>
 
             @for ($quarter = 1; $quarter <= 4; $quarter++)
                 {{-- Quarters 1 - 4 --}}
@@ -58,31 +58,43 @@
     {{-- tbody rows --}}
     <x-slot:tbody-rows>
         {{-- Summary row --}}
-        <tr>
-            <td class="mad-asp-table__tbody-td--year"><strong>{{ $record->year }}</strong></td>
-            <td class="mad-asp-table__tbody-td--mah-name"></td>
+        <tr class="mad-asp-table__tbody-summary-row">
+            <td class="mad-asp-table__tbody-td--year" colspan="2"><strong>{{ $record->year }}</strong></td>
 
             {{-- Summary year --}}
-            <td style="text-align: center">{{ $record->year_contract_plan }}</td>
-            <td style="text-align: center">{{ $record->year_contract_fact }}</td>
-            <td style="text-align: center">{{ $record->year_contract_fact_percentage }} %</td>
-            <td style="text-align: center">{{ $record->year_register_fact }}</td>
-            <td style="text-align: center">{{ $record->year_register_fact_percentage }} %</td>
+            <td style="background-color: var(--theme-table-td-background-color-1)">{{ $record->year_contract_plan }}</td>
+            <td style="background-color: var(--theme-table-td-background-color-1)">{{ $record->year_contract_fact }}</td>
+            <td style="background-color: var(--theme-table-td-background-color-1)">{{ $record->year_contract_fact_percentage }} %</td>
+            <td style="background-color: var(--theme-table-td-background-color-1)">{{ $record->year_register_fact }}</td>
+            <td style="background-color: var(--theme-table-td-background-color-1)">{{ $record->year_register_fact_percentage }} %</td>
 
             {{-- Summary Quarters 1 - 4 --}}
             @for ($quarter = 1, $monthIndex = 0; $quarter <= 4; $quarter++)
                 @if ($displayQuarters)
-                    <td>{{ $record->{'quarter_' . $quarter . '_contract_plan'} }}</td>
-                    <td>{{ $record->{'quarter_' . $quarter . '_contract_fact'} }}</td>
-                    <td>{{ $record->{'quarter_' . $quarter . '_register_fact'} }}</td>
+                    <td style="background-color: var(--theme-table-td-background-color-2)">{{ $record->{'quarter_' . $quarter . '_contract_plan'} }}</td>
+                    <td style="background-color: var(--theme-table-td-background-color-2)">{{ $record->{'quarter_' . $quarter . '_contract_fact'} }}</td>
+                    <td style="background-color: var(--theme-table-td-background-color-2)">{{ $record->{'quarter_' . $quarter . '_register_fact'} }}</td>
                 @endif
 
                 {{-- Summary months 1 - 12 --}}
                 @if ($displayMonths)
+                    @php
+                        $colors = [
+                            // Define the 3 repeating background colors
+                            'var(--theme-table-td-background-color-3)',
+                            'var(--theme-table-td-background-color-4)',
+                            'var(--theme-table-td-background-color-5)',
+                        ];
+                    @endphp
+
                     @for ($quarterMonths = 1; $quarterMonths <= 3; $quarterMonths++, $monthIndex++)
-                        <td>{{ $record->{$months[$monthIndex]['name'] . '_contract_plan'} }}</td>
-                        <td>{{ $record->{$months[$monthIndex]['name'] . '_contract_fact'} }}</td>
-                        <td>{{ $record->{$months[$monthIndex]['name'] . '_register_fact'} }}</td>
+                        @php
+                            $color = $colors[$monthIndex % 3]; // Cycle through colors
+                        @endphp
+
+                        <td style="background-color: {{ $color }}">{{ $record->{$months[$monthIndex]['name'] . '_contract_plan'} }}</td>
+                        <td style="background-color: {{ $color }}">{{ $record->{$months[$monthIndex]['name'] . '_contract_fact'} }}</td>
+                        <td style="background-color: {{ $color }}">{{ $record->{$months[$monthIndex]['name'] . '_register_fact'} }}</td>
                     @endfor
                 @endif
             @endfor
@@ -90,12 +102,8 @@
 
         {{-- Country rows --}}
         @foreach ($record->countries as $country)
-            <tr class="mad-asp-table__divider"></tr> {{-- Empty space as divider --}}
-
             <tr class="mad-asp-table__tbody-main-country-row">
-                <td class="mad-asp-table__tbody-td--country-name" style="text-align: left">
-                    <strong>{{ $country->code }}</strong>
-                </td>
+                <td class="mad-asp-table__tbody-td--country-name">{{ $country->code }}</td>
 
                 {{-- MAHs visibility toggler --}}
                 <td class="mad-asp-table__tbody-td--mah-name">
@@ -109,26 +117,38 @@
                 </td>
 
                 {{-- Country year --}}
-                <td>{{ $country->year_contract_plan }}</td>
-                <td>{{ $country->year_contract_fact }}</td>
-                <td>{{ $country->year_contract_fact_percentage }} %</td>
-                <td>{{ $country->year_register_fact }}</td>
-                <td>{{ $country->year_register_fact_percentage }} %</td>
+                <td style="background-color: var(--theme-table-td-background-color-1)">{{ $country->year_contract_plan }}</td>
+                <td style="background-color: var(--theme-table-td-background-color-1)">{{ $country->year_contract_fact }}</td>
+                <td style="background-color: var(--theme-table-td-background-color-1)">{{ $country->year_contract_fact_percentage }} %</td>
+                <td style="background-color: var(--theme-table-td-background-color-1)">{{ $country->year_register_fact }}</td>
+                <td style="background-color: var(--theme-table-td-background-color-1)">{{ $country->year_register_fact_percentage }} %</td>
 
                 {{-- Country Quarters 1 - 4 --}}
                 @for ($quarter = 1, $monthIndex = 0; $quarter <= 4; $quarter++)
                     @if ($displayQuarters)
-                        <td>{{ $country->{'quarter_' . $quarter . '_contract_plan'} }}</td>
-                        <td>{{ $country->{'quarter_' . $quarter . '_contract_fact'} }}</td>
-                        <td>{{ $country->{'quarter_' . $quarter . '_register_fact'} }}</td>
+                        <td style="background-color: var(--theme-table-td-background-color-2)">{{ $country->{'quarter_' . $quarter . '_contract_plan'} }}</td>
+                        <td style="background-color: var(--theme-table-td-background-color-2)">{{ $country->{'quarter_' . $quarter . '_contract_fact'} }}</td>
+                        <td style="background-color: var(--theme-table-td-background-color-2)">{{ $country->{'quarter_' . $quarter . '_register_fact'} }}</td>
                     @endif
 
                     {{-- Country Months 1 - 12 --}}
                     @if ($displayMonths)
+                        @php
+                            $colors = [
+                                // Define the 3 repeating background colors
+                                'var(--theme-table-td-background-color-3)',
+                                'var(--theme-table-td-background-color-4)',
+                                'var(--theme-table-td-background-color-5)',
+                            ];
+                        @endphp
+
                         @for ($quarterMonths = 1; $quarterMonths <= 3; $quarterMonths++, $monthIndex++)
-                            <td>{{ $country->{$months[$monthIndex]['name'] . '_contract_plan'} }}</td>
-                            <td>{{ $country->{$months[$monthIndex]['name'] . '_contract_fact'} }}</td>
-                            <td>{{ $country->{$months[$monthIndex]['name'] . '_register_fact'} }}</td>
+                            @php
+                                $color = $colors[$monthIndex % 3]; // Cycle through colors
+                            @endphp
+                            <td style="background-color: {{ $color }}">{{ $country->{$months[$monthIndex]['name'] . '_contract_plan'} }}</td>
+                            <td style="background-color: {{ $color }}">{{ $country->{$months[$monthIndex]['name'] . '_contract_fact'} }}</td>
+                            <td style="background-color: {{ $color }}">{{ $country->{$months[$monthIndex]['name'] . '_register_fact'} }}</td>
                         @endfor
                     @endif
                 @endfor
@@ -141,11 +161,11 @@
                     <td class="mad-asp-table__tbody-td--mah-name">{{ $mah->name }}</td>
 
                     {{-- MAH year --}}
-                    <td style="text-align: center">{{ $mah->year_contract_plan }}</td>
-                    <td style="text-align: center">{{ $mah->year_contract_fact }}</td>
-                    <td style="text-align: center">{{ $mah->year_contract_fact_percentage }} %</td>
-                    <td style="text-align: center">{{ $mah->year_register_fact }}</td>
-                    <td style="text-align: center">{{ $mah->year_register_fact_percentage }} %</td>
+                    <td>{{ $mah->year_contract_plan }}</td>
+                    <td>{{ $mah->year_contract_fact }}</td>
+                    <td>{{ $mah->year_contract_fact_percentage }} %</td>
+                    <td>{{ $mah->year_register_fact }}</td>
+                    <td>{{ $mah->year_register_fact_percentage }} %</td>
 
                     {{-- MAH Quarters 1 - 4 --}}
                     @for ($quarter = 1, $monthIndex = 0; $quarter <= 4; $quarter++)
