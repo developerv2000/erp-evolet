12f61029139bb056677d65184f43b62f68b16a90
MrBobPRO <bobnuridinov@gmal.com>
2025-02-25 15:50:04 +0500

    Product create dosage & pack fixed

--- resources/views/products/partials/create-form-dynamic-rows-list-item.blade.php
+++ resources/views/products/partials/create-form-dynamic-rows-list-item.blade.php
@@ -2,12 +2,12 @@
     <x-form.inputs.default-input
         class="specific-formatable-input"
         labelText="Dosage"
-        inputName="{{ 'products[' . $inputsIndex . '][pack]' }}" />
+        inputName="{{ 'products[' . $inputsIndex . '][dosage]' }}" />
 
     <x-form.inputs.default-input
         class="specific-formatable-input"
         labelText="Pack"
-        inputName="{{ 'products[' . $inputsIndex . '][dosage]' }}" />
+        inputName="{{ 'products[' . $inputsIndex . '][pack]' }}" />
 
     <div class="form-group">
         <x-form.misc.remove-row-button />
