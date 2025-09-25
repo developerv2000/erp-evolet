c512d227805477f4c0297fd25eb111c700a0380a
Murod <murod1bakhramov@gmail.com>
2025-09-25 15:45:10 +0500

    Product selection updated

--- app/Models/Process.php
+++ app/Models/Process.php
@@ -671,6 +671,7 @@ class Process extends BaseModel implements HasTitle, CanExportRecordsAsExcel, Pr
                 'currency_id',
                 'agreed_price',
                 'manufacturer_first_offered_price',
+                'our_first_offered_price',
                 'forecast_year_1',
                 'forecast_year_2',
                 'forecast_year_3',
